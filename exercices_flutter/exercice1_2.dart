309606c53db37ffa46065d41a14822b3717cccdf
Caleb-debug848 <calebdaony@gmail.com>
2025-11-20 23:58:08 +0100

    Create exercice1_2.dart

---
+++ exercices_flutter/exercice1_2.dart
@@ -0,0 +1,9 @@
+void main () {
+  //Déclaration des variables
+  String prenom = 'Caleb Daony';
+  int age = 20;
+  String profession = 'Etudiant';
+  // Utilisez l'interpolation $variable ou ${expression}
+  print('Je m\'appelle $prenom, j\'ai $age ans et je suis $profession.');
+  print('L\'annee prochaine j\'aurai ${age + 1} ans.');
+}
