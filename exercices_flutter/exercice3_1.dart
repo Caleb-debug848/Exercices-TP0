8db4d3d6b66497605334e2f41e7ce11de443a4db
Caleb-debug848 <calebdaony@gmail.com>
2025-11-21 01:04:24 +0100

    Add age classification logic in exercice3_1.dart

---
+++ exercices_flutter/exercice3_1.dart
@@ -0,0 +1,19 @@
+void main() {
+  int age = 16;
+  
+  if (age >= 18) {
+    print('Vous êtes majeur.');
+  } else {
+    print('Vous êtes mineur.');
+  }
+  
+  if (age >= 60) {
+    print('Vous êtes senior.');   
+    } else if (age >= 18) {
+      print('Vous êtes majeur.');
+    } else { 
+      print('Vous êtes mineur.');
+      }
+    }
+  
+
