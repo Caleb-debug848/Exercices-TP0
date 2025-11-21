b12c42a872b07eb7c951919d0ee581c8e37b0e1b
Caleb-debug848 <calebdaony@gmail.com>
2025-11-21 01:21:21 +0100

    Add switch case to print day of the week

---
+++ exercices_flutter/exercice3_3.dart
@@ -0,0 +1,29 @@
+void main() {
+  int jour = 3;
+
+  switch (jour) {
+    case 1:
+      print('Lundi');
+      break;
+    case 2:
+      print('Mardi');
+      break;
+    case 3:
+      print('Mercredi');
+      break;
+    case 4:
+      print('Jeudi');
+      break;
+    case 5:
+      print('Vendredi');
+      break;
+    case 6:
+      print('Samedi');
+      break;
+    case 7:
+      print('Dimanche');
+      break;
+    default:
+      print('Jour invalide');
+  }
+}
